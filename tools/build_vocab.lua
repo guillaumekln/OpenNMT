fd32189ee52a4f7f4f73142d7b1178496c41b7fd
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-06-29 14:08:23 +0200

    update build_vocab

--- tools/build_vocab.lua
+++ tools/build_vocab.lua
@@ -1,35 +1,25 @@
-require('onmt.init')
+local Vocabulary = require('onmt.data.Vocabulary')
+local TextFileIterator = require('onmt.data.iterators.TextFileIterator')
+local TextSplitter = require('onmt.data.transformers.TextSplitter')
 
-local cmd = onmt.utils.ExtendedCmdLine.new('build_vocab.lua')
+local ExtendedCmdLine = require('onmt.utils.ExtendedCmdLine')
+local Logger = require('onmt.utils.Logger')
+
 
 local options = {
   {
     '-data', '',
     [[Data file.]],
     {
-      valid = onmt.utils.ExtendedCmdLine.fileExists
+      valid = ExtendedCmdLine.fileExists
     }
   },
   {
     '-save_vocab', '',
     [[Vocabulary dictionary prefix.]],
     {
-      valid = onmt.utils.ExtendedCmdLine.nonEmpty
+      valid = ExtendedCmdLine.nonEmpty
     }
-  },
-  {
-    '-vocab_size', { 50000 },
-     [[List of source vocabularies size: `word[ feat1[ feat2[ ...] ] ]`.
-      If = 0, vocabularies are not pruned.]]
-  },
-  {
-    '-words_min_frequency', { 0 },
-    [[List of source words min frequency: `word[ feat1[ feat2[ ...] ] ]`.
-      If = 0, vocabularies are pruned by size.]]
-  },
-  {
-    '-keep_frequency', false,
-    [[Keep frequency of words in dictionary.]]
   },
   {
     '-idx_files', false,
@@ -37,24 +27,20 @@ local options = {
   }
 }
 
+Vocabulary.addOpts(options)
+
+local cmd = ExtendedCmdLine.new('build_vocab.lua')
 cmd:setCmdLineOptions(options, 'Vocabulary')
-onmt.utils.Logger.declareOpts(cmd)
+Logger.declareOpts(cmd)
 
-local function isValid(sent)
-  return #sent > 0
-end
+local opt = cmd:parse(arg)
 
-local function main()
-  local opt = cmd:parse(arg)
+_G.logger = Logger.new(opt.log_file, opt.disable_logs, opt.log_level)
 
-  _G.logger = onmt.utils.Logger.new(opt.log_file, opt.disable_logs, opt.log_level)
+local iterator = TextFileIterator.new(opt.data, opt.idx_files)
+local splitter = TextSplitter.new()
+local vocab = Vocabulary.new(opt, iterator, splitter)
 
-  local vocab = onmt.data.Vocabulary.init('source', opt.data, '', opt.vocab_size, opt.words_min_frequency, '', isValid, opt.keep_frequency, opt.idx_files)
+vocab:save(opt.save_vocab)
 
-  onmt.data.Vocabulary.save('source', vocab.words, opt.save_vocab .. '.dict')
-  onmt.data.Vocabulary.saveFeatures('source', vocab.features, opt.save_vocab)
-
-  _G.logger:shutDown()
-end
-
-main()
+_G.logger:shutDown()
